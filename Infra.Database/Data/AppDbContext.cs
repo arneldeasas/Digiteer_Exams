6153e2318cf0ffbcedcfa8afa26b4226e9bf309b
arneldeasas <113487190+arneldeasas@users.noreply.github.com>
2025-10-31 00:38:09 +0800

    feat(infra): Established entity relationships

--- Infra.Database/Data/AppDbContext.cs
+++ Infra.Database/Data/AppDbContext.cs
@@ -1,13 +1,21 @@
 ﻿using Application.TaskManager.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace Infra.Database.Data
+namespace Infra.Database.Data;
+
+public class AppDbContext : DbContext
 {
-	public class AppDbContext : DbContext
+	public AppDbContext(DbContextOptions<AppDbContext> options)
+		: base(options) { }
+	public DbSet<User> Users => Set<User>();
+	public DbSet<TaskItem> Tasks => Set<TaskItem>();
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
 	{
-		public AppDbContext(DbContextOptions<AppDbContext> options)
-			: base(options) { }
-		public DbSet<User> Users => Set<User>();
-		public DbSet<TaskItem> Tasks => Set<TaskItem>();
+		modelBuilder.Entity<User>()
+			.HasMany(u => u.Tasks)
+			.WithOne(t => t.User)
+			.HasForeignKey(t => t.UserId)
+			.OnDelete(DeleteBehavior.NoAction);
 	}
 }
