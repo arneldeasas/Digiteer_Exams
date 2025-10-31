b27b3d37ed88a04cd2f21c222884d29dd66b78a4
arneldeasas <113487190+arneldeasas@users.noreply.github.com>
2025-10-31 13:30:11 +0800

    feat(app): Created a custom validation attribute for dates in the past

---
+++ Application.TaskManager/CustomValidationAttributes/DateNotInPastAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.TaskManager.CustomValidationAttributes;
+
+[AttributeUsage(AttributeTargets.All, AllowMultiple = false)]
+internal class DateNotInPastAttribute : ValidationAttribute
+{
+	protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+	{
+		if (value is DateTime date && date < DateTime.Today)
+			return new ValidationResult($"{validationContext.DisplayName} cannot be in the past.");
+
+		return ValidationResult.Success;
+	}
+}
