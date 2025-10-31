581699b64d6718675402c1e08ec1df81fba45d53
arneldeasas <113487190+arneldeasas@users.noreply.github.com>
2025-10-31 14:16:48 +0800

    feat(app): Implementation of User Use-Cases

--- Application.TaskManager/DataTransferObjects/User/SignInFormDTO.cs
+++ Application.TaskManager/DataTransferObjects/User/SignInFormDTO.cs
@@ -1,7 +1,11 @@
-﻿namespace Application.TaskManager.DataTransferObjects.User;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.TaskManager.DataTransferObjects.User;
 
 public record SignInFormDTO
 {
+	[Required]
 	public string UserName { get; init; } = string.Empty;
+	[Required]
 	public string Password { get; init; } = string.Empty;
 }
--- Application.TaskManager/UseCases/Implementations/UserUc.cs
+++ Application.TaskManager/UseCases/Implementations/UserUc.cs
@@ -1,28 +1,40 @@
 ﻿using Application.TaskManager.DataTransferObjects.User;
 using Application.TaskManager.Models;
 using Application.TaskManager.Repositories;
+using Application.TaskManager.Services.Interfaces;
 using Application.TaskManager.UseCases.Interfaces;
 using Mapster;
 
 namespace Application.TaskManager.UseCases.Implementations;
 
-internal class UserUc(UseCaseHandler Handler, ICommandRepo Command, IReadRepo Read) : IUserUc
+internal class UserUc(UseCaseHandler Handler, ICommandRepo Command, IReadRepo Read,IPasswordService PasswordService) : IUserUc
 {
 	public Task SignInUser(SignInFormDTO form) =>
 		Handler.HandleUseCaseAsync(async () =>
 		{
-			TaskItem taskItem = form.Adapt<TaskItem>();
-			await Command.AddAsync(taskItem);
+			var user = await Read.FirstOrDefaultAsync<User>(x => x.UserName.Equals(form.UserName, StringComparison.CurrentCultureIgnoreCase));
+			if (user is null) throw new ApplicationException("Invalid username or password");
+
+			var passwordValid = PasswordService.VerifyPassword(form.Password, user.PasswordHash);
+			if (!passwordValid) throw new ApplicationException("Invalid username or password");
+
+		}, nameof(SignInUser), form);
+
+	public Task SignUpUser(UserFormDTO form) =>
+		Handler.HandleUseCaseAsync(async () =>
+		{
+			//Check for existing user
+			var userAlreadyExists= await Read.ExistsAsync<User>(x=>x.UserName.Equals(form.UserName, StringComparison.CurrentCultureIgnoreCase));
+			if(userAlreadyExists) throw new ApplicationException("User with the same username already exists");
+
+			User user = form.Adapt<User>();
+			user.PasswordHash = PasswordService.HashPassword(form.Password);
+
+			await Command.AddAsync(user);
 
 			await Command.SaveChangesAsync();
 
 			Command.UntrackAll();
 
 		}, nameof(SignInUser), form);
-
-	public Task SignUpUser(UserFormDTO form)
-	{
-		throw new NotImplementedException();
-	}
-
 }
