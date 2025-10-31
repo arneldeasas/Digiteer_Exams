53da5a89e0ab1bad66edb41353a2cbd3947aab2c
arneldeasas <113487190+arneldeasas@users.noreply.github.com>
2025-10-31 15:04:53 +0800

    feat: Created User endpoints

---
+++ backend/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Application.TaskManager.DataTransferObjects.User;
+using Application.TaskManager.UseCases.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace task_manager_api.Controllers
+{
+	[Route("user")]
+	[ApiController]
+	public class UserController(IUserUc Uc) : Controller
+	{
+		[HttpPost("sign-up")]
+		public async Task<IActionResult> SignUp([FromBody] UserFormDTO dto)
+		{
+			await Uc.SignUpUser(dto);
+			return Ok();
+		}
+
+		[HttpPost("sign-in")]
+		public async Task<IActionResult> SignIn([FromBody] SignInFormDTO dto)
+		{
+			await Uc.SignInUser(dto);
+			return Ok();
+		}
+	}
+}
