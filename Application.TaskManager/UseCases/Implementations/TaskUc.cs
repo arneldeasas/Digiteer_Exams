e477825c9dc1b28eba18333bb8903793568f0468
arneldeasas <113487190+arneldeasas@users.noreply.github.com>
2025-10-31 13:22:41 +0800

    feat(app): Created Handler / Wrapper for Use-Cases
    I created this so that I can centralized the entry point of each use-cases and for the logging to be convenient and clean.

--- Application.TaskManager/UseCases/Implementations/TaskUc.cs
+++ Application.TaskManager/UseCases/Implementations/TaskUc.cs
@@ -1,19 +1,22 @@
 ﻿using Application.TaskManager.DataTransferObjects.Task;
 using Application.TaskManager.UseCases.Interfaces;
+using System.Threading.Tasks;
 
 namespace Application.TaskManager.UseCases.Implementations;
 
-internal class TaskUc : ITaskUc
+internal class TaskUc(UseCaseHandler Handler) : ITaskUc
 {
-	public Task CompleteTask(int taskId)
-	{
-		throw new NotImplementedException();
-	}
+	public Task CreateTask(TaskFormDTO form)=>
+		Handler.HandleUseCaseAsync(async () =>
+		{
 
-	public Task CreateTask(TaskFormDTO form)
-	{
-		throw new NotImplementedException();
-	}
+		}, nameof(CompleteTask), form);
+
+	public Task CompleteTask(int taskId)=>
+		Handler.HandleUseCaseAsync(async () =>
+		{
+
+		}, nameof(CompleteTask), taskId);
 
 	public Task DeleteTask(TaskFormDTO form)
 	{
---
+++ Application.TaskManager/UseCases/UseCaseHandler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Application.TaskManager.UseCases;
+
+internal class UseCaseHandler(ILogger Logger)
+{
+	public async Task<T> HandleUseCaseAsync<T>(Func<Task<T>> useCaseAction, string useCaseName, params object[] args )
+	{
+		try
+		{
+			Logger.LogInformation("Started handling: {UseCaseName}", useCaseName);
+			Logger.LogDebug("Started handling: {UseCaseName} @{Arguments}", useCaseName,args);
+
+			var result = await useCaseAction();
+
+			Logger.LogInformation("Completed handling: {UseCaseName} ", useCaseName);
+			Logger.LogDebug("Completed handling: {UseCaseName} @{Result}", useCaseName,result);
+			return result;
+		}
+		catch (Exception ex)
+		{
+			Logger.LogError(ex, "Error occurred in use case: {UseCaseName}", useCaseName);
+			throw;
+		}
+	}
+}
