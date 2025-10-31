75489dc7e557f723ee742d3ef262dc01e195529c
arneldeasas <113487190+arneldeasas@users.noreply.github.com>
2025-10-31 14:08:59 +0800

    feat(app): Implementation of Password Service using Bcrypt

---
+++ Application.TaskManager/Services/Implementations/PasswordService.cs
@@ -0,0 +1,16 @@
+﻿using Application.TaskManager.Services.Interfaces;
+
+namespace Application.TaskManager.Services.Implementations;
+
+public class PasswordService : IPasswordService
+{
+	public string HashPassword(string password)
+	{
+		return BCrypt.Net.BCrypt.HashPassword(password);
+	}
+
+	public bool VerifyPassword(string hashedPassword, string providedPassword)
+	{
+		return BCrypt.Net.BCrypt.Verify(providedPassword, hashedPassword);
+	}
+}
