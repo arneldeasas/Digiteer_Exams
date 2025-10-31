4c7a4a2d0303575e0e354087108393ca40f89073
arneldeasas <113487190+arneldeasas@users.noreply.github.com>
2025-10-31 15:32:11 +0800

    feat: Added Task retrieval Use-Cases
    I Also adjusted CreateTask Use-case while at it.

--- Application.TaskManager/UseCases/Implementations/TaskUc.cs
+++ Application.TaskManager/UseCases/Implementations/TaskUc.cs
@@ -1,5 +1,4 @@
 ﻿using Application.TaskManager.DataTransferObjects.Task;
-using Application.TaskManager.Exceptions;
 using Application.TaskManager.Models;
 using Application.TaskManager.Repositories;
 using Application.TaskManager.UseCases.Interfaces;
@@ -11,10 +10,15 @@ namespace Application.TaskManager.UseCases.Implementations;
 
 internal class TaskUc(UseCaseHandler Handler, ICommandRepo Command, IReadRepo Read) : ITaskUc
 {
-	public Task CreateTask(TaskFormDTO form)=>
+	public Task CreateTask(TaskCreateFormDTO form) =>
 		Handler.HandleUseCaseAsync(async () =>
 		{
+			var userExists = await Read.ExistsAsync<User>(x => x.Id == form.UserId);
+			if (!userExists) throw new ApplicationException("User not found");
+
 			TaskItem taskItem = form.Adapt<TaskItem>();
+			taskItem.CreatedDate = DateTime.UtcNow;
+
 			await Command.AddAsync(taskItem);
 
 			await Command.SaveChangesAsync();
@@ -22,11 +26,11 @@ internal class TaskUc(UseCaseHandler Handler, ICommandRepo Command, IReadRepo Re
 			Command.UntrackAll();
 		}, nameof(CompleteTask), form);
 
-	public Task CompleteTask(int taskId)=>
+	public Task CompleteTask(int taskId) =>
 		Handler.HandleUseCaseAsync(async () =>
 		{
-			var taskItem =  await Read.FirstOrDefaultAsync<TaskItem>(x=>x.Id == taskId,track:true);
-			if(taskItem is null) throw new ApplicationException("Task not found");
+			var taskItem = await Read.FirstOrDefaultAsync<TaskItem>(x => x.Id == taskId, track: true);
+			if (taskItem is null) throw new ApplicationException("Task not found");
 
 			taskItem.FinishedDate = DateTime.UtcNow;
 			taskItem.UpdatedDate = DateTime.UtcNow;
@@ -59,4 +63,43 @@ internal class TaskUc(UseCaseHandler Handler, ICommandRepo Command, IReadRepo Re
 			await Command.SaveChangesAsync();
 			Command.UntrackAll();
 		}, nameof(StartTask), taskId);
+
+	public Task<TaskDTO?> GetTask(int taskId)=>
+		Handler.HandleUseCaseAsync(async () =>
+		{
+			var dto = await Read.GetProjectedAsync<TaskItem,TaskDTO>(
+				x=> x.Id == taskId,
+				x=> new TaskDTO
+				{
+					UserId = x.UserId,
+					Title = x.Title,
+					Description = x.Description,
+					DueDate = x.DueDate,
+					Id = x.Id,
+					StartDate = x.StartDate,
+					FinishedDate = x.FinishedDate,
+					Archived = x.Archived,
+				});
+
+			return dto;
+		}, nameof(GetTask), taskId);
+
+	public Task<IEnumerable<TaskListDTO>> GetAllUserTasks(int userId) =>
+		Handler.HandleUseCaseAsync(async () =>
+		{
+			var dto = await Read.GetManyProjectedAsync<TaskItem, TaskListDTO>(
+				x => x.UserId == userId,
+				x => new TaskListDTO
+				{
+					Title = x.Title,
+					Description = x.Description,
+					DueDate = x.DueDate,
+					Id = x.Id,
+					StartDate = x.StartDate,
+					FinishedDate = x.FinishedDate,
+					CreatedDate = x.CreatedDate
+				});
+
+			return dto.AsEnumerable();
+		}, nameof(GetAllUserTasks), userId);
 }
--- Application.TaskManager/UseCases/Interfaces/ITaskUc.cs
+++ Application.TaskManager/UseCases/Interfaces/ITaskUc.cs
@@ -4,8 +4,10 @@ namespace Application.TaskManager.UseCases.Interfaces;
 
 public interface ITaskUc
 {
-	Task CreateTask(TaskFormDTO form);
+	Task CreateTask(TaskCreateFormDTO form);
 	Task StartTask(int taskId);
 	Task CompleteTask(int taskId);
 	Task DeleteTask(int taskId);
+	Task<TaskDTO?> GetTask(int taskId);
+	Task<IEnumerable<TaskListDTO>> GetAllUserTasks(int userId);
 }
