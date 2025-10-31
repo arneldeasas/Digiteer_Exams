cf6a2f963a49daa79d63686d81d9bdc3d6db4e8b
arneldeasas <113487190+arneldeasas@users.noreply.github.com>
2025-10-31 20:09:03 +0800

    fix(frontend): added Missing Browser router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import SignUpPage from './pages/account/SignUpPage';
+import SignInPage from './pages/account/SignInPage';
 import './App.css'
 
 function App() {
   return (
 <Routes>
   <Route path="/" element={<SignInPage />} />
-  <Route path="/sign-up" element={<SignUpPage />} />
+  <Route path="/sign-up" element={<SignUpPage />}  />
 </Routes>
   );
 }
