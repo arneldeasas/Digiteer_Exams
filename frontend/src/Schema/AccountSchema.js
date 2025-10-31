8b582a239eb4cab0abc65496fc76e199c5a72db8
arneldeasas <113487190+arneldeasas@users.noreply.github.com>
2025-10-31 20:07:34 +0800

    feat(frontend): Added schema for accounts module
    -added schema for signin
    -added schema for signup

---
+++ frontend/src/Schema/AccountSchema.js
@@ -0,0 +1,22 @@
+import * as yup from "yup";
+export const SignInSchema = yup
+  .object({
+    username: yup.string().required("Username is required :P"),
+    password: yup.string().required("Password is required :P"),
+  })
+  .required();
+
+export const SignUpSchema = yup
+  .object({
+    username: yup.string().required("Username is required :P"),
+    name: yup.string().required("Password is required :P").min(3),
+    email: yup
+      .string()
+      .required("Password is required :P")
+      .email("Invalid email format"),
+    password: yup
+      .string()
+      .required("Password is required :P")
+      .min(6, "Password must be at least 6 characters"),
+  })
+  .required();
