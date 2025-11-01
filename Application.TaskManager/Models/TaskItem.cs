0b964374c287977752ae357227eb3eeec1b14a9d
arneldeasas <113487190+arneldeasas@users.noreply.github.com>
2025-11-01 19:48:38 +0800

    update(app&infra): Nullable task start date

--- Application.TaskManager/Models/TaskItem.cs
+++ Application.TaskManager/Models/TaskItem.cs
@@ -5,7 +5,7 @@ public class TaskItem
 	public int Id { get; set; }
 	public string Title { get; set; } = string.Empty;
 	public string Description { get; set; } = string.Empty;
-	public DateTime StartDate { get; set; }
+	public DateTime? StartDate { get; set; }
 	public DateTime DueDate { get; set; }
 	public DateTime? FinishedDate { get; set; }
 	public DateTime CreatedDate { get; set; }
