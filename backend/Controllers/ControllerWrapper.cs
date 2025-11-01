8852cd7e331a84b5afcd7e96690133aa498b3145
arneldeasas <113487190+arneldeasas@users.noreply.github.com>
2025-11-01 13:07:33 +0800

    feat(api): Controller Base Wrapper
    I created a base controller so that I can consistently return proper status and results for each requests.

---
+++ backend/Controllers/ControllerWrapper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace task_manager_api.Controllers;
+
+public class ControllerWrapper :ControllerBase
+{
+	async Task<IActionResult> ExecuteRequestAsync(Func<Task> action)
+	{
+		try
+		{
+			await action();
+			return Ok();
+		}
+		catch (ApplicationException ex)
+		{
+			return BadRequest(ex);
+		}
+		catch (Exception ex)
+		{
+			return Problem(ex.InnerException?.Message ?? "Server error occured");
+		}
+	}
+	async Task<IActionResult> ExecuteRequestAsync<T>(Func<Task<T>> action)
+	{
+		try
+		{
+			var result = await action();
+			return Ok(result);
+		}
+		catch (ApplicationException ex)
+		{
+			return BadRequest(ex);
+		}
+		catch (Exception ex)
+		{
+			return Problem(ex.InnerException?.Message ?? "Server error occured");
+		}
+	}
+}
