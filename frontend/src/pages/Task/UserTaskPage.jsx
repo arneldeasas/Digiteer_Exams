f5739f8d4cc681a198efe85aee832c02a3a1789a
arneldeasas <113487190+arneldeasas@users.noreply.github.com>
2025-11-01 16:53:44 +0800

    feat(frontend): Added create task button

--- frontend/src/pages/Task/UserTaskPage.jsx
+++ frontend/src/pages/Task/UserTaskPage.jsx
@@ -1,11 +1,29 @@
-import { Typography } from "@mui/material";
+import { Add, Assignment } from "@mui/icons-material";
+import { Typography, Card, Chip, IconButton, Button } from "@mui/material";
+import TaskCardMenu from "../../Components/TaskCardMenu";
+import TaskCard from "../../Components/TaskCard";
 
 function UserTaskPage() {
   return (
     <div className="w-screen h-screen bg-gray-100 flex flex-col items-center">
-      <Typography variant="h4" className="mt-10">
-        User Tasks
-      </Typography>
+      <Card className="max-w-[1440px] w-full m-10 p-10">
+        <div className="flex justify-between">
+          <Typography variant="h6">User Tasks</Typography>
+          <div>
+            <Button
+              variant="contained"
+              size="small"
+              className="mr-4"
+              startIcon={<Add />}
+            >
+              Create Task
+            </Button>
+          </div>
+        </div>
+        <div className="mt-4 flex flex-wrap">
+          <TaskCard task={{}} />
+        </div>
+      </Card>
     </div>
   );
 }
